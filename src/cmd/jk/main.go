b7bbb08b011be89927daf6a013d24fe633a6f6c8
Casey Robinson <casey@rampantmonkey.com>
2015-08-13 11:38:58 -0400

    create container struct to encapsulate lxc operations

--- src/cmd/jk/main.go
+++ src/cmd/jk/main.go
@@ -16,11 +16,81 @@ func init() {
 	rand.Seed(time.Now().UTC().UnixNano())
 }
 
-func main() {
-	ips := startContainers(numContainers)
+type container struct {
+	name string
+	ip string
+}
 
-	for _, ip := range ips {
-		fmt.Println(string(ip))
+func (c *container) Start() bool {
+	// check that container exists and is not running
+	cmd := exec.Command("sudo", "lxc-info", "-n", c.name, "-s")
+	info, err := cmd.Output()
+	if err != nil {
+		log.Fatalf("[error]: %s does not exist. create container and retry.\n", c.name)
+		return false
+	}
+	if strings.Contains(string(info), "RUNNING") { // TODO: handle other container states
+		log.Printf("[info]: container named %s already running.\n", c.name)
+	} else {
+		cmd = exec.Command("sudo", "lxc-start", "-n", c.name)
+		err = cmd.Run()
+		if err != nil {
+			log.Fatalf("[error]: could not start %s\n", c.name)
+			return false
+		}
+	}
+
+	c.findIp()
+	return true
+}
+
+var ipRegexp = regexp.MustCompile(`(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\/24`)
+// not perfect, but close enough. allows for things like 442.0.0.0 the goal is not to verify that ip addr provides valid ip addresses though.
+func (c *container) findIp() {
+	retriesRemaining := 3
+retry:
+	cmd := exec.Command("sudo", "lxc-attach", "-n", c.name, "--", "ip", "-4", "addr", "show", "eth0")
+	ipaddrOut, err := cmd.Output()
+	if err != nil {
+		log.Fatalf("[error]: could not attach to %s\n", c.name)
+	}
+
+	match := ipRegexp.FindSubmatch(ipaddrOut)
+	if match == nil && retriesRemaining >= 0 {
+		retriesRemaining--
+		time.Sleep(300 * time.Millisecond)
+		goto retry
+	}
+	c.ip = string(match[1])
+}
+
+func (c *container) Stop() {
+	cmd := exec.Command("sudo", "lxc-stop", "-n", c.name)
+	err := cmd.Run()
+	if err != nil {
+		log.Printf("[error]: failed to stop %s\n", c.name)
+	}
+}
+
+func main() {
+	var containers []*container
+	for i := 0; i < numContainers; i++ {
+		containers = append(containers, &container{name: fmt.Sprintf("n%d", i)})
+	}
+
+	for _, c := range containers {
+		started := c.Start()
+		if !started {
+			log.Printf("[error]: failed to start %s\n", c.name)
+		}
+	}
+
+	for _, c := range containers {
+		log.Printf("%s\n", c.ip)
+	}
+
+	for _, c := range containers {
+		c.Stop()
 	}
 
 	// set up main executors and task generator
@@ -31,78 +101,6 @@ func main() {
 	}
 
 	time.Sleep(10 * time.Second)
-
-	stopContainers(numContainers)
-}
-
-// Assumes that containers exist and are named n0, n1, ..., nn
-func stopContainers(n int) {
-	for i := 0; i < n; i++ {
-		name := fmt.Sprintf("n%d", i)
-
-		cmd := exec.Command("sudo", "lxc-stop", "-n", name)
-		err := cmd.Run()
-		if err != nil {
-			log.Printf("[error]: failed to stop %s\n", name)
-		}
-	}
-}
-
-// Assumes that containers exist and are named n0, n1, ..., nn
-func startContainers(n int) [][]byte {
-	for i := 0; i < n; i++ {
-		name := fmt.Sprintf("n%d", i)
-
-		// check that container exists and is not running
-		cmd := exec.Command("sudo", "lxc-info", "-n", name, "-s")
-		info, err := cmd.Output()
-		if err != nil {
-			log.Fatalf("[error]: %s does not exist. create container and retry.\n", name)
-			return nil
-		}
-		if strings.Contains(string(info), "RUNNING") { // TODO: handle other container states
-			log.Printf("[info]: container named %s already running.\n", name)
-			continue
-		}
-
-		// start container
-		cmd = exec.Command("sudo", "lxc-start", "-n", name)
-		err = cmd.Run()
-		if err != nil {
-			log.Fatalf("[error]: could not start %s\n", name)
-			return nil
-		}
-	}
-
-	var ips [][]byte
-	for i := 0; i < n; i++ {
-		name := fmt.Sprintf("n%d", i)
-		ips = append(ips, getIp(name))
-	}
-	return ips
-}
-
-// not perfect, but close enough. allows for things like 442. the goal is not to verify that ip addr provides valid ip addresses though.
-var ipRegexp = regexp.MustCompile(`(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\/24`)
-func getIp(name string) []byte {
-	retriesRemaining := 3
-retry:
-	cmd := exec.Command("sudo", "lxc-attach", "-n", name, "--", "ip", "-4", "addr", "show", "eth0")
-	ipaddrOut, err := cmd.Output()
-	if err != nil {
-		log.Fatalf("[error]: could not attach to %s\n", name)
-	}
-	if len(ipaddrOut) == 0 && retriesRemaining >= 0 {
-		retriesRemaining--
-		time.Sleep(300 * time.Millisecond)
-		goto retry
-	}
-
-	match := ipRegexp.FindSubmatch(ipaddrOut)
-	if match == nil {
-		return nil
-	}
-	return match[1]
 }
 
 func startLog() chan status {
